f522a352fdafae2911ac760d1c3c2223ba2e7f65
Mudo <mustafadogruer50@gmail.com>
2022-02-26 22:00:13 +0100

    Added buildingPyramid.py

---
+++ buildingPyramid.py
@@ -0,0 +1,14 @@
+blocks = int(input("Enter the number of blocks: "))
+
+#This code calculates height of a triangle pyramid which it is
+#consist of  layers have 1 more block than preceding layer
+
+
+left_blocks = blocks
+height = 0
+while left_blocks > height:
+    height += 1
+    left_blocks -= height
+
+
+print("The height of the pyramid:", height)
